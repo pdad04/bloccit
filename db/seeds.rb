68fc2de86c2dcd057066cd9fe03f3963c28e4a45
Andre <andre_dean@me.com>
2017-12-21 20:49:45 -0800

    add user authorization to posts and topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,10 +36,17 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: "andre_dean@me.com",
-  password: "helloworld"
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'hellowworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
 )
 
 puts "Seed finished"
